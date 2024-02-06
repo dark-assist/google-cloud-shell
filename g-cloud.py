006e8f5c8b1e7ef844776b548447d4eadb19ed16
D4rk_4ssi$T <108276894+dark-assist@users.noreply.github.com>
2024-02-06 09:23:38 +0530

    g-cloud.py

--- g-cloud.py
+++ g-cloud.py
@@ -2,8 +2,9 @@ import os
 import subprocess
 import urllib.request
 import tarfile
-import subprocess
+
 os.chdir('/data/data/com.termux/files/home')
+
 def print_color(text, color='\033[94m'):
     print(f"{color}{text}\033[0m")
 
@@ -12,21 +13,9 @@ def run_command(command):
     subprocess.run(command, check=True)
 
 def download_file(url, destination):
-    # Function to download a file from a given URL with a progress bar
+    # Function to download a file from a given URL
     with urllib.request.urlopen(url) as response, open(os.path.join(os.path.expanduser("~"), destination), 'wb') as out_file:
-        file_size = int(response.headers['Content-Length'])
-        chunk_size = 1024 * 1024  # 1 MB
-        bytes_so_far = 0
-
-        print_color(f"Downloading {destination}...")
-
-        for data in iter(lambda: response.read(chunk_size), b''):
-            out_file.write(data)
-            bytes_so_far += len(data)
-            progress = bytes_so_far * 100 / file_size
-            print_color(f"Progress: {progress:.2f}%", end='', color='\033[94m')
-            print('\033[0m', end='\r')  # Reset color and move to the beginning of the line
-            print()  # Move to the next line after download completion
+        out_file.write(response.read())
 
 def extract_tar(file_path, output_dir):
     # Function to extract a tar.gz file to a specified directory
@@ -62,7 +51,7 @@ extract_tar("google-cloud-cli-458.0.1-linux-arm.tar.gz", ".")
 print_color("Adding the gcloud CLI to your path")
 run_command(["./google-cloud-sdk/install.sh"])
 
-print_color("Initialising the gcloud CLI")
+print_color("Initializing the gcloud CLI")
 run_command(["./google-cloud-sdk/bin/gcloud", "init"])
 
 # Setting up alias in bash.bashrc
@@ -71,4 +60,3 @@ alias_command = "alias google='gcloud alpha cloud-shell ssh'"
 append_to_bashrc(bashrc_path, alias_command)
 
 print_color("Now exit and reopen your Termux, then type 'google' to start Google shell")
-            
