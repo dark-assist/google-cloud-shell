2d5bf12367e1b71c48831be2e19100b25143ee0a
D4rk_4ssi$T <108276894+dark-assist@users.noreply.github.com>
2024-04-02 17:58:47 +0530

    Add files via upload

---
+++ google-shell.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+cd $HOME
+echo "Made by @sanatani_x_anonymous"
+echo "Telegram:-https://t.me/temuxhacking"
+apt update >/dev/null
+apt install curl python openssh openssl-y >/dev/null
+
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-470.0.0-linux-arm.tar.gz > .down.txt
+echo "Decompressing Google Cloud SDK file"
+tar -xvzf google-cloud-cli-470.0.0-linux-arm.tar.gz
+echo "Adding the gcloud CLI to your path"
+sleep 2 
+echo "If Question Asked Just Hit Enter"
+sleep 5
+./google-cloud-sdk/install.sh
+echo "Initializing the gcloud CLI"
+./google-cloud-sdk/bin/gcloud init
+
+echo "alias google='gcloud alpha cloud-shell ssh'" > .bashrc
+clear
+echo "Now exit and reopen your Termux, then type 'google' to start Google shell" 
+sleep 7
+exit
